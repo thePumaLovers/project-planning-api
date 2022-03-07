9744e252456f20dc50fc6fa749bcd0ff17293b4b
Matt Powell <mpowell171@gmail.com>
2022-03-07 18:35:37 -0500

    began working on projectRoutes, ran into snag subdoc implementation Co-authored-by: @aidynhillyard @rosscarnegie @JAndrzGutierrez

--- controllers/projectRoutes.js
+++ controllers/projectRoutes.js
@@ -3,7 +3,16 @@ const router = express.Router()
 
 const Group = require('./../models/Group')
 
+// Create new project
+router.post('/', async (req, res) => {
+    const newProject = await Group.children.push({projects: req.body})
 
+    res.json({
+        status: 200,
+        message: 'new group created',
+        newProject: newProject,
+    })
+})
 
 
 module.exports = router;
