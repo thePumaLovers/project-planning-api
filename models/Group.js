574ac922de8a2281abce27ea2f8b133193f0dfb2
Matt Powell <mpowell171@gmail.com>
2022-03-07 17:24:56 -0500

    created models folder, created Group.js (projects subdoc needs to be adjusted) Co-authored-by: @aidynhillyard @rosscarnegie @JAndrzGutierrez

---
+++ models/Group.js
@@ -0,0 +1,13 @@
+//import connection
+const mongoose = require('./../db/connection')
+const Schema = mongoose.Schema
+
+//schema
+const groupSchema = new Schema ({
+    displayName: String,
+    location: String,
+    projects: []
+})
+
+//export model
+module.exports = mongoose.model('Group', groupSchema)
