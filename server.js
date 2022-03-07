862457fe473f5051ffafe965597f333f3a1486fc
Matt Powell <mpowell171@gmail.com>
2022-03-07 17:38:13 -0500

    added skeletpon of groupRoutes.js and controller variable in server.js Co-authored-by: @aidynhillyard @rosscarnegie @JAndrzGutierrez

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
-require('dotenv').config()
-const PORT = process.env.PORT
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors');
-const app = express()
+// Imports
+require("dotenv").config();
+const PORT = process.env.PORT;
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const app = express();
 
 // Middleware
-app.use(logger('dev'));
-app.use(cors())
+app.use(logger("dev"));
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        status: 200,
-        msg: 'default route and nothing to see here'
-    })
-})
+app.get("/", (req, res) => {
+  res.json({
+    status: 200,
+    msg: "default route and nothing to see here",
+  });
+});
 
+// Controllers
+const groupRouter = require("./controllers/groupRoutes");
+app.use("/groups", groupRouter);
 
-app.listen(PORT, () => console.log('Server running on port 4000!'))
+app.listen(PORT, () => console.log("Server running on port 4000!"));
